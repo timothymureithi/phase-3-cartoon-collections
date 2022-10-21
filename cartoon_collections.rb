c75671639bf34f640a785d583f5ab41d7b55fac7
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-21 13:28:28 +0300

    Completed lesson

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+dwarves.map.with_index do |dwarves, index|
+  puts "#{index+1}. #{dwarves}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |planeteer_calls|
+    planeteer_calls.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+    if call.length > 4
+      true
+    else
+      false
+      end
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_finder = cheese_types.find do |type|
+    food_list.include?(type)
+end
+cheese_finder
 end
